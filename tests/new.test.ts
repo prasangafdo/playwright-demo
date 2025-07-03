72420cd148230409358b23310e73b09101c336c9
Prasanga Mac <prasangafdz@outlook.com>
2025-07-03 16:33:18 +0530

    Completed login

--- tests/new.test.ts
+++ tests/new.test.ts
@@ -9,7 +9,11 @@ test("Login test demo",async()=>{
 
     await page.goto("https://ecommerce-playground.lambdatest.io/")
     await page.hover("//li[@class='nav-item dropdown dropdown-hoverable']//span[@class ='title' and normalize-space()='My account']")
-    await page.click("//span[normalize-space()='Login']")
-    await assert()
+    await page.click("//span[normalize-space()='Login']") // We can use text=Login or "'Login'" to find the element based on the text.
+    await page.fill("#input-email", "prasangafdz@gmail.com")
+    await page.fill("#input-password", "1qaz!QAZ")
+    await page.click("//input[@value='Login']")
+
+    await page.waitForTimeout(5000)
 
 })
