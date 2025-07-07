daf11ddacc1cc8324155d7ffe51190c974ce600f
Prasanga Mac <prasangafdz@outlook.com>
2025-07-07 21:27:47 +0530

    Added more context and pages

--- tests/new.test.ts
+++ tests/new.test.ts
@@ -5,7 +5,7 @@ import { assert } from "console"
 test("Login test demo",async()=>{
     let browser = await chromium.launch()
     let context = await browser.newContext()
-    let page = await context.newPage()
+    let page = await context.newPage() // Similar to a tab
 
     await page.goto("https://ecommerce-playground.lambdatest.io/")
     await page.hover("//li[@class='nav-item dropdown dropdown-hoverable']//span[@class ='title' and normalize-space()='My account']")
@@ -15,5 +15,18 @@ test("Login test demo",async()=>{
     await page.click("//input[@value='Login']")
 
     await page.waitForTimeout(5000)
+
+    let newPage = await context.newPage() // Will create a new tab on the same browser.
+
+    await newPage.goto("https://ecommerce-playground.lambdatest.io/")
+
+    let newSession = await browser.newContext() // Will open a new browser session
+    let newSessionPage = await newSession.newPage()
+    await newSessionPage.goto("https://ecommerce-playground.lambdatest.io/")
+
+    await newSessionPage.waitForTimeout(5000)
+
+
+
 
 })
