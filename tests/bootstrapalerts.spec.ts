ec34dac0662a787a549409b44c5ab1a9b3aabb17
Prasanga Mac <prasangafdz@outlook.com>
2025-09-10 19:56:11 +0530

    Included automated code for additional bootstrap alerts

--- tests/bootstrapalerts.spec.ts
+++ tests/bootstrapalerts.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from "@playwright/test";
 
-test.only("Verify bootstrap alert",async({page})=>{
+test("Verify bootstrap alert",async({page})=>{
     
     const btnLunchModal = page.locator("//button[@data-target='#myModal']")
     const btnCloseModal = page.locator("//button[text()='Save Changes']").nth(0)
@@ -8,5 +8,20 @@ test.only("Verify bootstrap alert",async({page})=>{
     page.goto("https://www.lambdatest.com/selenium-playground/bootstrap-modal-demo")
 
     await btnLunchModal.click()
+    await btnCloseModal.click()
+})
+
+test.only("Verify additional bootstrap alert",async({page})=>{
+    
+    const btnLunchModal = page.locator("//button[@data-target='#myMultiModal']")
+    const btnLunchSecondModal = page.locator("//button[@data-target='#mySecondModal']")
+    const btnCloseModal = page.locator("//button[text()='Save Changes']").nth(1)
+    const btnCloseSecondModal = page.locator("//button[text()='Save Changes']").nth(2)
+
+    page.goto("https://www.lambdatest.com/selenium-playground/bootstrap-modal-demo")
+
+    await btnLunchModal.click()
+    await btnLunchSecondModal.click()
+    await btnCloseSecondModal.click()
     await btnCloseModal.click()
 })
