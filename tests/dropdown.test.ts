0a44ef636c98f1735f856f11d1a920b9aeed94b5
Prasanga Mac <prasangafdz@outlook.com>
2025-09-13 19:34:00 +0530

    Removed unnecessary comment

--- tests/dropdown.test.ts
+++ tests/dropdown.test.ts
@@ -17,16 +17,3 @@ test("Verify multi select drop down",async({page})=>{
     ])
     await page.waitForTimeout(3000)
 })
-
-
-
-
-// test("Verify multi select drop down",async({page})=>{
-//     await page.goto("https://www.lambdatest.com/selenium-playground/select-dropdown-demo")
-//     await page.selectOption("#multi-select",{
-//         {value:"Florida"}
-//     {index:3}
-//        {label:"Texas"}
-//     })
-
-// })
