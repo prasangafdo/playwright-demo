631fab42eb733dd6f16a1f4c58cd1f17934fcf64
Prasanga Mac <prasangafdz@outlook.com>
2025-09-14 16:13:05 +0530

    Fixed the element locator failure

--- tests/dropdown.test.ts
+++ tests/dropdown.test.ts
@@ -17,3 +17,14 @@ test("Verify multi select drop down",async({page})=>{
     ])
     await page.waitForTimeout(3000)
 })
+
+test("Verify JQuery drop down", async({page})=>{
+    await page.goto("https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo")
+    await page.click("#country+span")
+    await page.locator("ul#select2-country-results").locator("li",{
+            hasText: "India"
+        }).click()
+        await page.waitForTimeout(3000)
+    }
+    
+)
